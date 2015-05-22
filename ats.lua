51253e6e12d2195e2a6b3c8d193147ae0242b3c5
Soni L. <EnderMoneyMod@gmail.com>
2015-05-22 17:52:44 +0000

    Negative time is totally a thing

--- ats.lua
+++ ats.lua
@@ -4,13 +4,8 @@
 --]]
 local function _round(_i)
   _i = _i * 20;
-  if _i >= 0 then
-    local x = math.floor(_i+.5)
-    return x / 20, x
-  else
-    local x = math.ceil(_i-.5)
-    return x / 20, x
-  end
+  local x = math.floor(_i+.5)
+  return x / 20, x
 end
 
 -- custom "working" sleep (can sleep 4-12 ticks without sleeping an extra tick)
